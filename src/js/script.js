b52801f885a8f2c0e3f11dafed8b4ca03750d833
KubaGithub92 <jakub.rosol@email.cz>
2023-01-09 01:46:31 +0100

    hamburger menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+// Selecting elements
+const btnNavEl = document.querySelector(".header__button-nav");
+const headerEl = document.querySelector(".header");
+const navLinks = document.querySelectorAll(".header__nav-link");
+
+// Opening and closing navigation after clicking the button
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+// Closing navigation after clicking nav link
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    console.log(link);
+    headerEl.classList.toggle("nav-open");
+  });
+});
